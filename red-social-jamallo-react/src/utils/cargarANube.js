fde9827bd2fd778fb6fd9f9d7de8242051cb7f60
jamallo <alba.trapaga@gmail.com>
2024-06-02 01:06:29 +0200

    02  de junio de 2024 - AWS

--- red-social-jamallo-react/src/utils/cargarANube.js
+++ red-social-jamallo-react/src/utils/cargarANube.js
@@ -1,14 +1,14 @@
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({
-  accessKeyId: '905418119496',
+  accessKeyId: '',
   secretAccessKey: '',
   region: 'eu-west-3',
 });
 
 export const cargarANube = async (file) => {
   const params = {
-    Bucket: 'YOUR_BUCKET_NAME',
+    Bucket: 'jamallo',
     Key: `images/${file.name}`,
     Body: file,
     ACL: 'public-read',
